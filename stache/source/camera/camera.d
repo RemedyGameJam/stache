40cbe2d3a83745556462a1f1c96db76e561bb233
Ethan Watson <gooberman@gmail.com>
2012-10-13 23:50:47 +0300

    Let's see what's going on here.

---
+++ stache/source/camera/camera.d
@@ -0,0 +1,21 @@
+module stache.camera.camera;
+
+import stache.i.entity;
+
+class Camera
+{
+	void Apply()
+	{
+/*		float x = MFDeg2Rad!60;
+		MFView_ConfigureProjection(x, 0.01, 100000);
+		// TODO: Nasty singletonses
+		float ratio = Game.Instance.mfInitParams.display.displayRect.width / Game.Instance.mfInitParams.display.displayRect.height;
+		MFView_SetAspectRatio(ratio);
+		MFView_SetProjection();
+
+		MFMatrix mat;
+
+		mat.t = MFVector(5, 2, 0, 1);
+		MFView_SetCameraMatrix(mat);*/
+	}
+}
